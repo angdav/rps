3395e3bbf632f694196daf190af20a0196036b53
David Ang <cdang1024@gmail.com>
2020-02-08 17:42:56 -0500

    finished basic functionality, added images

--- index.js
+++ index.js
@@ -5,23 +5,46 @@ function getRPS(){
 }
 
 function outputRPS(choice){
-    cpuText.innerHTML = getRPS();
+    let gen = getRPS();
+
+    cpuText.innerHTML = gen;
     playerText.innerHTML = choice;
+    playerImage.src = "img/"+choice+".png";
+    cpuImage.src = "img/"+gen+".png";
 }
 
+function displayWinner(){
+    cpu = cpuText.innerHTML;
+    player = playerText.innerHTML;
+
+    if(cpu === player){
+        alert("tie!");
+    } else if ((cpu === 'rock' && player === 'scissors') || (cpu === 'paper' && player === 'rock') || (cpu === 'scissors' && player === 'paper')) {
+        alert("cpu wins!");
+    } else {
+        alert("player wins!");
+    }
+
+}
 
 let cpuText = document.getElementById('cpuText');
+let cpuImage = document.getElementById('cpuImage');
 let playerText = document.getElementById('playerText');
+let playerImage = document.getElementById('playerImage');
+
 let rockb = document.getElementById('rockb');
 let paperb = document.getElementById('paperb');
 let scissorsb = document.getElementById('scissorsb');
 
 rockb.addEventListener('click', function(){
     outputRPS("rock");
+    displayWinner();
 });
 paperb.addEventListener('click', function(){
     outputRPS("paper");
+    displayWinner();
 });
 scissorsb.addEventListener('click', function(){
     outputRPS("scissors");
+    displayWinner();
 });
